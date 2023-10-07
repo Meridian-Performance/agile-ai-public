a3466dde25b7aeda4430ce92bafeae8280019513
Cody J. Phillips <kedifei@gmail.com>
2023-10-06 22:37:23 -0700

    Implement resolve

--- agile_ai/processing/processor.py
+++ agile_ai/processing/processor.py
@@ -3,15 +3,25 @@ from agile_ai.processing.processor_io import IO
 
 class Processor:
     def perform_super(self, inputs: IO):
-        key_part = inputs.get_key()
-        outputs: IO = self.get_outputs_cls()()
-        outputs.init_options(key_part)
+        outputs = self._get_outputs(inputs)
         self.perform(inputs, outputs)
         outputs.store_options()
         return outputs
 
+    def _get_outputs(self, inputs: IO):
+        key_part = inputs.get_key()
+        outputs: IO = self.get_outputs_cls()()
+        outputs.init_options(key_part)
+        return outputs
+
     def perform(self, inputs, outputs):
         raise NotImplementedError
+
+    def resolve(self, inputs: IO) -> IO:
+        outputs = self._get_outputs(inputs)
+        if outputs.all_options_present():
+            return outputs
+        return self.perform_super(inputs)
 
     @classmethod
     def get_outputs_cls(cls) -> IO:
--- agile_ai_tests/processor/processing_test.py
+++ agile_ai_tests/processor/processing_test.py
@@ -5,21 +5,12 @@ from agile_ai.memoization.object_option import ObjectOption
 from agile_ai.memoization.warehouse_service import WarehouseService
 from agile_ai.processing.processor import Processor
 from agile_ai.processing.processor_io import IO
-from agile_ai_tests.test_helpers.pyne_test_helpers import before_each, TCBase, describe, it
+from agile_ai_tests.test_helpers.pyne_test_helpers import before_each, TCBase, describe, it, with_stubs
 from agile_ai_tests.test_helpers.test_helpers import reset_and_configure_test
 from pynetest.pyne_tester import pyne
+from pynetest.test_doubles.attached_spy import AttachedSpy, attach_spy
 from pynetest.test_doubles.stub import MegaStub
 from pynetest.expectations import expect
-
-
-class TestContext(TCBase):
-    __services__: Marker
-    warehouse_service: WarehouseService
-
-    __stubs__: Marker
-    stubs: MegaStub
-
-    __other__: Marker
 
 
 class SomeInputA(WarehouseObject):
@@ -52,6 +43,18 @@ class ProcessorA(Processor):
         outputs.some_output_b.set(SomeOutputB())
 
 
+class TestContext(TCBase):
+    __services__: Marker
+    warehouse_service: WarehouseService
+
+    __stubs__: Marker
+    stubs: MegaStub
+    perform_stub: AttachedSpy
+
+    __other__: Marker
+    inputs: ProcessorA.Inputs
+    processor: ProcessorA
+
 @pyne
 def processing_test():
     @before_each(TestContext)
@@ -59,21 +62,22 @@ def processing_test():
         reset_and_configure_test()
         tc.warehouse_service.set_warehouse_directory(tc.test_directory / "warehouse")
         tc.warehouse_service.set_partition_name("some_partition_name")
+        some_input_a = SomeInputA().with_key_part(KeyLiteral("some_input_a"))
+        some_input_b = SomeInputA().with_key_part(KeyLiteral("some_input_b"))
+        tc.warehouse_service.put_object(some_input_a)
+        tc.warehouse_service.put_object(some_input_b)
+        inputs = ProcessorA.Inputs()
+        inputs.some_input_a = ObjectOption(some_input_a)
+        inputs.some_input_b = ObjectOption(some_input_b)
+        tc.inputs = inputs
 
     @describe("#perform (super)")
     def _():
         @it("calls perform")
         def _(tc: TestContext):
-            some_input_a = SomeInputA().with_key_part(KeyLiteral("some_input_a"))
-            some_input_b = SomeInputA().with_key_part(KeyLiteral("some_input_b"))
-            tc.warehouse_service.put_object(some_input_a)
-            tc.warehouse_service.put_object(some_input_b)
-            inputs = ProcessorA.Inputs()
-            inputs.some_input_a = ObjectOption(some_input_a)
-            inputs.some_input_b = ObjectOption(some_input_b)
-            outputs: ProcessorA.Outputs = ProcessorA().perform_super(inputs)
+            outputs: ProcessorA.Outputs = ProcessorA().perform_super(tc.inputs)
             expect(outputs).to_be_a(ProcessorA.Outputs)
-            key_part = inputs.get_key()
+            key_part = tc.inputs.get_key()
             expect(outputs.some_output_a.object_key).to_be(ObjectKey(SomeOutputA, key_part))
             expect(outputs.some_output_b.object_key).to_be(ObjectKey(SomeOutputB, key_part))
             expect(tc.warehouse_service.has_object(outputs.some_output_a.object_key)).to_be(True)
@@ -81,6 +85,31 @@ def processing_test():
 
     @describe("#resolve")
     def _():
-        @it("")
+        @before_each
         def _(tc: TestContext):
-            ...
+            tc.processor = ProcessorA()
+            tc.stubs = attach_spy(tc.processor, "perform")
+
+        @describe("when all outputs exist")
+        def _():
+            @before_each
+            def _(tc: TestContext):
+                key_part = tc.inputs.get_key()
+                tc.warehouse_service.put_object(SomeOutputA().with_key_part(key_part))
+                tc.warehouse_service.put_object(SomeOutputB().with_key_part(key_part))
+
+            @it("doesn't call perform")
+            @with_stubs
+            def _(tc: TestContext):
+                outputs = tc.processor.resolve(tc.inputs)
+                expect(outputs).to_be_a(ProcessorA.Outputs)
+                expect(tc.processor.perform).was_not_called()
+
+        @describe("when any outputs is missing")
+        def _():
+            @it("calls perform")
+            @with_stubs
+            def _(tc: TestContext):
+                outputs = tc.processor.resolve(tc.inputs)
+                expect(outputs).to_be_a(ProcessorA.Outputs)
+                expect(tc.processor.perform).was_called()
