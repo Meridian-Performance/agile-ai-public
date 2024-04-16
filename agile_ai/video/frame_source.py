e5c98a848a495795e7513877328d74f4d7dec3ec
Cody J. Phillips <kedifei@gmail.com>
2024-04-16 12:05:36 -0700

    Return self in populate frames, add ext

--- agile_ai/video/frame_source.py
+++ agile_ai/video/frame_source.py
@@ -6,19 +6,22 @@ from agile_ai.data_marshalling.directory_path import DirectoryPath
 class FrameSource:
     frame_directory: DirectoryPath
     frames: Iterable
+    ext: str
 
-    def __init__(self, frame_directory: DirectoryPath):
+    def __init__(self, frame_directory: DirectoryPath, ext="png"):
         self.frame_directory = frame_directory
+        self.ext = ext
 
     def with_frames(self, frames: Iterable):
         self.frames = frames
         return self
 
-    def populate_frames(self, ext="png"):
+    def populate_frames(self):
         for i, frame in enumerate(self.frames):
             print("Saving frame", i)
-            (self.frame_directory // f"frame_{i:008d}.{ext}").put(frame)
+            (self.frame_directory // f"frame_{i:008d}.{self.ext}").put(frame)
+        return self
 
     def get_input_path(self):
-        input_path = self.frame_directory // "frame_%08d.png"
+        input_path = self.frame_directory // f"frame_%08d.{self.ext}"
         return input_path
