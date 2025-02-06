4f7a4ea05906c2161237355a6f1e825c3e59cff3
Cody J. Phillips <kedifei@gmail.com>
2025-02-06 08:18:40 -0800

    Fix lazy / memo option typing on get

--- agile_ai/utilities/lazy_option.py
+++ agile_ai/utilities/lazy_option.py
@@ -1,6 +1,6 @@
-from agile_ai.utilities.option import Option
+from agile_ai.utilities.option import Option, OptionT
 
 
-class LazyOption(Option):
-    def get(self):
+class LazyOption(Option[OptionT]):
+    def get(self) -> OptionT:
         return Option.get(self)()
--- agile_ai/utilities/memo_option.py
+++ agile_ai/utilities/memo_option.py
@@ -1,16 +1,18 @@
-from agile_ai.utilities.option import Option
+from agile_ai.utilities.option import Option, OptionT
+
 
 class NoMemo:
     pass
 
-class MemoOption(Option):
+
+class MemoOption(Option[OptionT]):
     _memo: any
 
     def __init__(self, value):
         Option.__init__(self, value)
         self._memo = NoMemo
 
-    def get(self):
+    def get(self) -> OptionT:
         if self._memo == NoMemo:
             self._memo = Option.get(self)()
         return self._memo
