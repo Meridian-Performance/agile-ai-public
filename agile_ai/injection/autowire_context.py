32b2748a5759ce8b7e181c1e719963ea1acc0937
Cody J. Phillips <kedifei@gmail.com>
2025-05-25 11:38:45 -0700

    Add inject function and tests

--- agile_ai/injection/autowire_context.py
+++ agile_ai/injection/autowire_context.py
@@ -11,5 +11,10 @@ class AutowireContext:
     def get_service(self, cls):
         if cls not in self.instance_by_cls:
             instance = cls()
-            self.instance_by_cls[cls] = instance
+            self.inject(instance, cls)
         return self.instance_by_cls[cls]
+
+    def inject(self, instance, for_cls=None):
+        if for_cls is None:
+            for_cls = instance.__class__
+        self.instance_by_cls[for_cls] = instance
